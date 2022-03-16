40a3e37b1602babd4d0fa3d7b2a16b58e799487d
ohm <destiny300940@gmail.com>
2022-03-16 14:29:39 +0700

    adbanner

--- src/components/main/adbanner.vue
+++ src/components/main/adbanner.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>ad banner</div>
+  <div>
+    ad banner
+    <!-- mobile  -->
+    <div class="mobile-only lt-sm"></div>
+    <!-- tablet  -->
+    <div class="mobile-only gt-xs"></div>
+    <!-- pc  -->
+    <div
+      class="desktop-only shadow-6"
+      style="max-width: 1600px; width: 100%; margin: auto"
+    ></div>
+  </div>
 </template>
 
 <script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <head-bar></head-bar>
+    <ad-banner></ad-banner>
   </div>
 </template>
 
 <script>
 import headBar from "../components/main/headbar.vue";
+import adBanner from "../components/main/adbanner.vue";
 export default {
   components: {
     headBar,
+    adBanner,
   },
 };
 </script>
